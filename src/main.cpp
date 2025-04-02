d350d06e091f13a45d8d600c2dbacaab10ce3432
caoruoxuan <2100017739@stu.pku.edu.cn>
2025-04-02 15:41:30 +0800

    add -debug mode

--- src/main.cpp
+++ src/main.cpp
@@ -15,11 +15,13 @@ using namespace std;
 extern FILE *yyin;
 extern int yyparse(unique_ptr<BaseAST> &ast);
 
+string mode = "-debug";
+
 int main(int argc, const char *argv[]) {
   // 解析命令行参数. 测试脚本/评测平台要求你的编译器能接收如下参数:
   // compiler 模式 输入文件 -o 输出文件
   assert(argc == 5);
-  auto mode = argv[1];
+  mode = argv[1];
   auto input = argv[2];
   auto output = argv[4];
 
@@ -30,7 +32,7 @@ int main(int argc, const char *argv[]) {
   // 调用 parser 函数, parser 函数会进一步调用 lexer 解析输入文件的
   unique_ptr<BaseAST> ast;
   auto ret = yyparse(ast);
-  // assert(!ret);
+  assert(!ret);
 
 
   // dump AST
